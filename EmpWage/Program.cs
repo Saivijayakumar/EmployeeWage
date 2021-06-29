ddf8d0d06bbc397838b34f291e362640f3020577
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-06-29 15:08:51 +0530

    Code for UC2

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Here Emp means Employee
+            int EmpPresent = 1;
+            int EmpHours = 0;
+            int EmpRatePerHour = 20;
+            int EmpWage = 0;
+
+            //UC1 Finding Employee Present or Absent by using Random class
+            //Creating object or instance of Random class
+            Random random = new Random();
+
+            //For random values useing Next() method
+            int EmpInput = random.Next(0, 2);
+            if (EmpInput == EmpPresent)
+            {
+                Console.WriteLine("Employee is Present");
+                EmpHours = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            //UC2 finding salary of employee for one day
+            //Applying formula for finding out employee salary
+            EmpWage = EmpRatePerHour * EmpHours;
+            Console.WriteLine("Employee wage per day =" + EmpWage);
+            Console.Read();
+        }
+    }
+}
