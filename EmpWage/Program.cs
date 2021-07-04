70ebdc86475c0e1b8b09bed16db04c62d7c1076b
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-04 11:30:08 +0530

    [SaiVijayaKumar] Ability to save total wage for each company

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -8,42 +8,14 @@ namespace EmpWage
 {
     class Program
     {
-        //Initializing Constant Values
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-
-        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays,int maxHoursPerMonth)
-        {
-            //Local Variables
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            //Computation
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-            }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total Emp Wage for company : " + company + "is: " + totalEmpWage);
-            return totalEmpWage;
-        }
         static void Main(string[] args)
         {
-            computeEmpWage("Airtel",20,4,10);
-            computeEmpWage("Idea", 10, 4, 30);
+            EmpWageBuilderObject airtel = new EmpWageBuilderObject("Airtel", 20, 4, 10);
+            EmpWageBuilderObject idea = new EmpWageBuilderObject("Idea", 10, 5, 30);
+            airtel.computeEmpWage();
+            Console.WriteLine(airtel.toString());
+            idea.computeEmpWage();
+            Console.WriteLine(idea.toString());
             Console.Read();
         }
     }
